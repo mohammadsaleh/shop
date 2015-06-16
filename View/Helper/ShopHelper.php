5811fd8d5eb9207bdf83116c93e4fa134fe6cf22
mohammadsaleh <mohammadsaleh.sayari@gmail.com>
2015-06-16 23:27:34 +0430

    category page

--- View/Helper/ShopHelper.php
+++ View/Helper/ShopHelper.php
@@ -23,12 +23,6 @@ class ShopHelper extends AppHelper{
         foreach($productsBlks as $alias => $options){
             $content = str_replace($content, $this->productList($alias, $options), $content);
         }
-        if(empty($productsBlks)){
-            $categoriesBlks = $this->_stringConverter->parseString('categories|c', $content);
-            foreach($categoriesBlks as $alias => $options){
-                $content = str_replace($content, $this->categoriesTreePanel($alias, $options), $content);
-            }
-        }
         return $content;
     }
 
@@ -40,17 +34,6 @@ class ShopHelper extends AppHelper{
         $output = $this->_View->element($options['element'], array(
             'alias' => $alias,
             'productList' => $this->_View->viewVars['products_for_layout'][$alias],
-            'options' => $options
-        ));
-        return $output;
-    }
-    private function categoriesTreePanel($alias, $options = array()){
-        $options = array_merge(array(
-            'element' => 'Shop.categories_tree_panel'
-        ), $options);
-        $output = $this->_View->element($options['element'], array(
-            'alias' => $alias,
-            'categoriesTree' => $this->_View->viewVars['subCategories_tree_panel_for_layout'][$alias],
             'options' => $options
         ));
         return $output;
